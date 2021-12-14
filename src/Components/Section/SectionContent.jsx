cd558cde5acfae88ae0845bee2ddaf5733e798ae
Vishroy <vishroy.seenarain@hangarww.com>
2021-12-14 10:47:16 +0400

    Correction on section content render

--- src/Components/Section/SectionContent.jsx
+++ src/Components/Section/SectionContent.jsx
@@ -7,6 +7,22 @@ const SectionContent = () => {
   let data = [
     {
       id: 1,
+      title: 'Discover New Opportunities',
+      paragraph: 'The world’s business information in one place, filterable and sortable. Mattermark makes it faster and easier to source the best deals.',
+      link: '',
+      image: 'https://d1mfvzd5g3mg00.cloudfront.net/dist/images/opportunity.png',
+      classes: ''
+    },
+    {
+      id: 2,
+      title: 'Track Success',
+      paragraph: 'Search company and investor profiles for deep insights into their growth and performance over time. Break the cycle of doing business without good data.',
+      link: '',
+      image: 'https://d1mfvzd5g3mg00.cloudfront.net/dist/images/growth.png',
+      classes: 'flex-row-reverse'
+    },
+    {
+      id: 3,
       title: 'Automatically Enrich Leads In Salesforce',
       paragraph: 'Get actionable insights by syncing over 80 informative fields to help warm leads and power conversations from first touch to deal close. Get all the relevant information from inside Salesforce, and apply triggers and actions to any lead, opportunity, or other object.',
       link: 'https://mattermark.com/salesforce/',
@@ -14,7 +30,7 @@ const SectionContent = () => {
       classes: ''
     },
     {
-      id: 2,
+      id: 4,
       title: 'Customize Your Experience With API',
       paragraph: 'Our API provides data on millions of companies. In just minutes, you can integrate Mattermark with your products, systems, and business processes. Create your own models and scoring algorithms to fit your business.',
       link: 'https://mattermark.com/api/',
@@ -27,11 +43,11 @@ const SectionContent = () => {
   return (
     <>
       {
-        data.map(({title, ...content}) => {
+        data.map(({id, ...content}) => {
           return(
-            <section className="sect-content">
+            <section key={id} className="sect-content">
               <div className="container">
-                <CardContent title={title} {...content} />
+                <CardContent {...content} />
               </div>
             </section>
           )
